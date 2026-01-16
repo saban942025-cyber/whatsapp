c6d5a49ff651f4c80f2556b0bd86aa6990feaf98
saban942025-cyber <saban942025@gmail.com>
2026-01-16 20:00:18 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,10 +3,7 @@ const nextConfig = {
   output: 'export',      // <--- חובה! זה מייצר את תיקיית "out" הסטטית
   images: {
     unoptimized: true,   // חובה! מונע שבירת תמונות בייצוא סטטי
-  },
-  eslint: {
-    ignoreDuringBuilds: true, // מונע עצירה של הבנייה בגלל שטויות
-  },
+
 };
 
 export default nextConfig;
