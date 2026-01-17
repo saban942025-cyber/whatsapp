4b2e00f45ef8ba8148586dd9db03c2b60db3ff65
saban942025-cyber <saban942025@gmail.com>
2026-01-17 03:09:22 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* ההגדרות החדשות לא כוללות את המפתח eslint כאן */
   typescript: {
-    // זה עדיין נתמך ועוזר לעבור Build אם יש שגיאות טייפ
     ignoreBuildErrors: true,
   },
 };
